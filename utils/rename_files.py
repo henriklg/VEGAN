6ca2daab25f3b5153ebd402abead67a4e6f38b45
ingridut <42763463+ingridut@users.noreply.github.com>
2019-09-27 17:35:39 +0200

    Add test for compatability

--- utils/rename_files.py
+++ utils/rename_files.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import Image
 
 directory = sys.argv[1]
 img_type = sys.argv[2]
@@ -7,6 +8,12 @@ filenames = os.listdir(directory)
 
 counter = 1
 for filename in filenames: 
-	new_name = img_type + '_' + str(counter) + '.' + filename.split('.')[-1]
-	os.rename(directory + '/' + filename, directory + '/' + new_name)
-	counter += 1
+	file = directory + '/' + filename
+	try:
+		Image.open(file)
+		new_name = img_type + '_' + str(counter) + '.' + filename.split('.')[-1]
+		os.rename(file, directory + '/' + new_name)
+		counter += 1
+	except:
+		print('Deleted ', file)
+		os.remove(file)
