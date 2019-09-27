7fb3a87ebad3ee8ee2597743cd4b5d529456db6f
Henrik Gjestang <henriklg@fys.uio.no>
2019-09-27 18:32:46 +0200

    commit

--- utils/rename_files.py
+++ utils/rename_files.py
@@ -1,11 +1,12 @@
 import os
 import sys
 
-directory = sys.argv[0]
+directory = sys.argv[1]
+img_type = sys.argv[2]
 filenames = os.listdir(directory)
 
 counter = 1
 for filename in filenames: 
-	new_name = directory + '_' + counter + filename.split('.')[-1]
-	os.rename(filename, new_name)
+	new_name = img_type + '_' + str(counter) + '.' + filename.split('.')[-1]
+	os.rename(directory + '/' + filename, directory + '/' + new_name)
 	counter += 1
